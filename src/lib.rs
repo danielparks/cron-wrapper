170e59697e65a849ebf5e64cefa8cd6cbaceb3c5
Daniel Parks <dp+git@oxidized.org>
2023-05-08 20:53:31 -0700

    Add missing documentation and enable lint
    This enables the `missing_docs` lint for the entire crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![warn(missing_docs)]
+//! Miscellaneous useful modules that support cron-wrapper.
+
+/// Read output from child process as events.
 pub mod command;
+
+/// Optionally buffer all writes to a stream until we decide to unpause it.
 pub mod pause_writer;
+
+/// Stateful timeouts.
 pub mod timeout;
