58c7d49c08fdfdc52b233a4e45b454755dcf2975
Daniel Parks <dp+git@oxidized.org>
2024-03-05 14:07:51 -0800

    Add `--lock-file PATH` option to `run`
    This causes `cron-wrapper` to generate a lock file and fail fast if the
    lock file is already locked by another process.
    
    Part of #91 — Option to ensure only one job is run at once.

---
+++ tests/lock.rs
@@ -0,0 +1,43 @@
+use assert2::check;
+use bstr::ByteSlice;
+use std::thread;
+use std::time::Duration;
+use tempfile::NamedTempFile;
+
+mod helpers;
+
+#[test]
+fn lock_conflict() {
+    let lock_path_handle = NamedTempFile::new().unwrap().into_temp_path();
+    let lock_path = lock_path_handle.to_path_buf();
+    let lock_path2 = lock_path_handle.to_path_buf();
+
+    let handle = thread::spawn(move || {
+        let output = helpers::run(["run", "--lock-file"])
+            .arg(&lock_path)
+            .args(["sleep", "0.5"])
+            .output()
+            .unwrap();
+        check!(output.status.success());
+        check!(output.stdout.as_bstr() == "");
+        check!(output.stderr.as_bstr() == "");
+    });
+
+    thread::sleep(Duration::from_millis(100));
+
+    let handle2 = thread::spawn(move || {
+        let output = helpers::run(["run", "--lock-file"])
+            .arg(&lock_path2)
+            .args(["sleep", "0.5"])
+            .output()
+            .unwrap();
+        check!(!output.status.success(), "Should fail to obtain lock");
+        check!(output.stdout.as_bstr() == "");
+        check!(output
+            .stderr
+            .starts_with(b"Error: Lock owned by another process"));
+    });
+
+    handle.join().unwrap();
+    handle2.join().unwrap();
+}
